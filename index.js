a43a68215a2f5ab0bc9118796df98ff7309848b5
Harvey Mortimore <77932367+hmortimore69@users.noreply.github.com>
2025-01-28 21:24:08 +0000

    Completed js201

--- index.js
+++ index.js
@@ -13,20 +13,85 @@
 'use strict';
 
 function id() {
-  // e.g. return "UP654321";
+  return "UP2203185";
 }
 
 function fn() {
-  return 'Replace This With Your First Name';
+  return "Harvey";
 }
 
 function sn() {
-  return 'Replace This With Your Surname';
+  return "Mortimore";
 }
 
-function example() {
-  // replace this example with
-  // your first function then
-  // add more below as necessary.
+function add(a, b) {
+  return a + b;
+}
 
+function subtract(a, b) {
+  return a - b;
+}
+
+function checkObject(obj) {
+  obj.checked = true;
+}
+
+function checkObjectInside(obj) {
+  if (obj.data) {
+    obj.data.checked = true;
+  }
+}
+
+function arraySet(arr, i, n) {
+  if (arr[i]) {
+    arr[i] = n;
+  }
+}
+
+function addAll(arr) {
+  return arr.reduce((accumulator, curr) => accumulator + curr, 0);
+}
+
+function larger(a, b) {
+  return a > b ? a : b;
+}
+
+function largest(arr) {
+  if (arr.length === 0) return null;
+  return arr.reduce((largest, curr) => curr > largest ? curr : largest, arr[0]);
+}
+
+function compare(a, b) {
+  if (a.length !== b.length) return false;
+  return a.every((value, index) => value === b[index]);
+}
+
+function addToAll(arr, n) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] += n;
+  }
+
+  return arr;
+}
+
+let remembered = "";
+function rememberThis(keepsake) {
+  remembered = keepsake;
+}
+
+function nArray(n) {
+  return Array.from({ length: n }, (_, i) => i + 1);
+}
+
+function addAllOpt(arr) {
+  if (!arr || arr.length === 0) return 0;
+  return arr.reduce((accumulator, curr) => accumulator + curr, 0);
+}
+
+function divisors(arr, div) {
+  return arr.filter(num => num % div === 0);
+}
+
+function multiples(n, m) {
+  return Array.from({ length: n }, (_, i) => (i + 1) * m);
 }
